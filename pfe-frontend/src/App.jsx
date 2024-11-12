092941c562c98686afcdd0fe5505cc189f6b1c83
aissaouifatimazahra <149192817+aissaouifatimazahra@users.noreply.github.com>
2024-11-12 12:42:53 -0800

    just try

--- pfe-frontend/src/App.jsx
+++ pfe-frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div><p>hellllllllllllllooooo</p>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
