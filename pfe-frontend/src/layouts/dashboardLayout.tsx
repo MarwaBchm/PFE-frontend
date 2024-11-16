a32037e946ad10608695cedb4aed6573ba13c94b
MarwaBchm <168100326+MarwaBchm@users.noreply.github.com>
2024-11-16 15:14:42 -0800

    up8

--- pfe-frontend/src/layouts/dashboardLayout.tsx
+++ pfe-frontend/src/layouts/dashboardLayout.tsx
@@ -13,7 +13,7 @@ const Dashboard = () => {
         <Navbar />
         {/* Main Content */}
         <main className="flex-1 p-2 pt-4 w-full h-full bg-blue-3">
-          <div className="bg-white p-2 rounded-xl flex h- overflow-y-auto shadow-sm ">
+          <div className=" p-2  flex  overflow-y-auto  ">
             <Outlet /> {/* This renders the nested routes */}
           </div>
         </main>
