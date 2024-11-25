cf9c143ee711faf7553781d8302619a4ab20400c
MarwaBchm <168100326+MarwaBchm@users.noreply.github.com>
2024-11-24 16:19:09 -0800

    up20

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import AppRoutes from "./plugins/routes";
-
-function App() {
- 
-  return <div><AppRoutes/></div>;
-}
-
-export default App;
--- src/components/emails.jsx
+++
@@ -1,8 +0,0 @@
-// Emails.jsx
-import React from "react";
-
-const Emails = () => {
-  return <h1>Welcome to the Emails Page</h1>;
-};
-
-export default Emails;
